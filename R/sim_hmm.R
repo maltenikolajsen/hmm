550e08ec1c3fc4a523e5de4d134b69735764c18e
asbjornholk <asbjornholk@gmail.com>
2021-11-04 11:41:59 +0100

    Added internal function for simulating HMM's

---
+++ R/sim_hmm.R
@@ -0,0 +1,37 @@
+#' sim_hmm
+#'
+#' @description A function for simulating HMM's. Not expected to be called by user, use instead the 'simulate' method for hmm objects.
+#'
+#' @param n Integer. Length of hmm
+#' @param delta m-dimensional vector. Initial distribution of Markov chain
+#' @param Gamma m x m matrix. Transition probs for Markov chain
+#' @param rdists List of functions f_i, where f_i(k) generates k i.i.d. copies of X|C=i
+#' @param include_state Logical. Denotes whether or not the hidden states should be included. If TRUE, they are appended to the end of the HMM.
+#'
+#' @return Vector of simulated HMM
+#'
+sim_hmm <- function(n, delta, Gamma, rdists, include_state=FALSE){
+
+  # Set m for notation
+  m <- length(delta)
+
+  # First, generate C_1,...,C_n
+  C <- numeric(n)
+  C[1] <- sample(m, prob=delta)
+
+  for(i in 2:n){
+    C[i] <- sample(m, prob=Gamma[C[i-1], ])
+  }
+
+  # Next, simulate X_i's
+  X <- numeric(n)
+  for(j in 1:m){
+    X[C == j] <- rdists[[j]](sum(C == j))
+  }
+
+  if(include_state){
+    return(c(X, C))
+  }
+
+  return(X)
+}
