5948334ec0116a85a7e7b5b307934b9069e3a84f
Malte Nikolajsen <malte.nikolajsen.privat@gmail.com>
2021-11-12 00:07:33 +0100

    Correction for simulation.

--- R/simulate.R
+++ R/simulate.R
@@ -4,7 +4,7 @@ simulate.hmm <- function(model, nsim = 1, seed = NULL, ...){
   set.seed(NULL)
   hasArg(include_state) || (include_state = F)
 
-  out <- matrix(NA, nrow = nsim, ncol = ifelse(include_state, model$n, 2*model$n))
+  out <- matrix(NA, nrow = nsim, ncol = ifelse(include_state, 2*model$n, model$n))
   for(j in 1:nsim){
     out[j,] <- sim_hmm(model$n, model$delta, model$Gamma, model$rdists, include_state = F)
   }
